1460812632413f8a7705da3d15cc6981168ebf74
Michael Wilbert Gunawan <michaelwilbert11@gmail.com>
2024-05-02 13:27:09 +0700

    Update packer.lua

--- lua/em_weh/plugins/packer.lua
+++ lua/em_weh/plugins/packer.lua
@@ -1,8 +1,3 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -11,42 +6,47 @@ return require('packer').startup(function(use)
   -- use 'lukas-reineke/indent-blankline.nvim'
   use 'tpope/vim-commentary'
   -- use {'neoclide/coc.nvim', branch = 'master', run = 'yarn install --frozen-lockfile'}
-  use {
-      'VonHeikemen/lsp-zero.nvim',
-      requires = {
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},
-          {'williamboman/mason.nvim'},
-          {'williamboman/mason-lspconfig.nvim'},
+  -- use {
+  --     'VonHeikemen/lsp-zero.nvim',
+  --     requires = {
+  --         -- LSP Support
+  --         {'neovim/nvim-lspconfig'},
+  --         {'williamboman/mason.nvim'},
+  --         {'williamboman/mason-lspconfig.nvim'},
 
-          -- Autocompletion
-          {'hrsh7th/nvim-cmp'},
-          {'hrsh7th/cmp-buffer'},
-          {'hrsh7th/cmp-path'},
-          {'saadparwaiz1/cmp_luasnip'},
-          {'hrsh7th/cmp-nvim-lsp'},
-          {'hrsh7th/cmp-nvim-lua'},
+  --         -- Autocompletion
+  --         {'hrsh7th/nvim-cmp'},
+  --         {'hrsh7th/cmp-buffer'},
+  --         {'hrsh7th/cmp-path'},
+  --         {'saadparwaiz1/cmp_luasnip'},
+  --         {'hrsh7th/cmp-nvim-lsp'},
+  --         {'hrsh7th/cmp-nvim-lua'},
 
-          -- Snippets
-          {'L3MON4D3/LuaSnip'},
-          {'rafamadriz/friendly-snippets'},
-      }
-  }
+  --         -- Snippets
+  --         {'L3MON4D3/LuaSnip'},
+  --         {'rafamadriz/friendly-snippets'},
+  --     }
+  -- }
   use 'edkolev/tmuxline.vim'
   -- LSP Utils
   use 'preservim/tagbar'
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  -- use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   -- Git Utils
   use 'tpope/vim-fugitive'
   use 'airblade/vim-gitgutter'
-  use 'rbong/vim-flog'
+  use {'rbong/vim-flog', branch = 'v1'}
   -- Fuzzy Finder
-  use ('junegunn/fzf', {['do'] =  vim.fn['fzf#install']})
-  use 'junegunn/fzf.vim'
+  -- use ('junegunn/fzf', {['do'] =  vim.fn['fzf#install']})
+  -- use 'junegunn/fzf.vim'
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+-- or                            , branch = '0.1.x',
+                                   requires = { {'nvim-lua/plenary.nvim'} }
+  }
   -- Aesthetic Plugins
   use 'vim-airline/vim-airline'
   use 'ryanoasis/vim-devicons'
   -- Colorscheme
   use 'tomasiser/vim-code-dark'
+  use 'folke/tokyonight.nvim'                       
 end)
-
