650f5a6fd9c58923df6cc265a39d2276149230e4
Michael Wilbert Gunawan <michaelwilbert11@gmail.com>
2024-05-02 17:14:34 +0700

    Update remap.lua

--- lua/em_weh/core/remap.lua
+++ lua/em_weh/core/remap.lua
@@ -16,7 +16,10 @@ nnoremap('<leader>f', '<cmd>NERDTreeFind<CR>')
 nmap('<F8>' ,'<cmd>TagbarToggle<CR>')
 
 -- FuzzyFinder
-nnoremap('<leader>p', '<cmd>Files<CR>')
+nnoremap('<leader>p', '<cmd>Telescope find_files<CR>')
+nnoremap('<leader>g', '<cmd>Telescope live_grep<CR>')
+nnoremap('<leader>b', '<cmd>Telescope buffers<CR>')
+nnoremap('<leader>h', '<cmd>Telescope help_tags<CR>')
 
 -- COC
 -- Go to Definition
